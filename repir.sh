d90db992cf9f6fa5654b91dd737a87e3bb946f5f
RED-VIROUS7 <68949718+RED-VIROUS7@users.noreply.github.com>
2020-10-02 13:29:03 +0200

    Add files via upload

---
+++ repir.sh
@@ -0,0 +1,41 @@
+cd
+git clone https://github.com/MFDGaming/ubuntu-in-termux
+cd ubuntu-in-termux
+bash ubuntu.sh
+cd ubuntu-fs
+cd etc
+rm resolv.conf
+cd
+cd repir
+ls
+mv resolv.conf $HOME
+cd
+mv resolv.conf ubuntu-in-termux/ubuntu-fs/etc
+cd ubuntu-in-termux/ubuntu-fs/etc
+chmod +x resolv.conf
+chmod 777 resolv.conf
+cd
+cd ubuntu-in-termux
+pkg install figlet
+figlet R E D V I R O U S
+sleep 0.1
+figlet R
+sleep 0.1
+figlet E
+sleep 0.1
+figlet D
+sleep 0.1
+figlet V
+sleep 0.1
+figlet I
+sleep 0.1
+figlet R
+sleep 0.1
+figlet O
+sleep 0.1
+figlet U
+sleep 0.1
+figlet S
+sleep 0.1
+sleep 2
+bash startubuntu.sh
